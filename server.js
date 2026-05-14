fbe0e695679cf7834571f8db1604e15cfb86a1b9
simba211 <dinocutieboy@gmail.com>
2026-05-15 00:12:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,25 @@
 const { addonBuilder, serveHTTP } = require("stremio-addon-sdk");
 
 const manifest = {
-    id: "org.koc.vnaddon",
-    version: "1.0.0",
-    name: "VN Movie Sources",
-    description: "Addon cho OPhim / PhimAPI / NguonC",
-    resources: ["stream"],
-    types: ["movie", "series"],
-    idPrefixes: ["tt"]
+  id: "org.vn.test",
+  version: "1.0.0",
+  name: "VN Test",
+  description: "VN addon",
+  resources: ["stream"],
+  types: ["movie"],
+  idPrefixes: ["tt"],
+  catalogs: []
 };
 
 const builder = new addonBuilder(manifest);
 
-builder.defineStreamHandler(async ({ type, id }) => {
+builder.defineStreamHandler(() => ({
+  streams: [
+    {
+      title: "Test Stream",
+      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    }
+  ]
+}));
 
-    // TODO:
-    // Tự map IMDb -> slug phim VN
-
-    return {
-        streams: [
-            {
-                title: "OPhim",
-                url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-            },
-            {
-                title: "PhimAPI",
-                url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-            },
-            {
-                title: "NguonC",
-                url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-            }
-        ]
-    };
-});
-
-serveHTTP(builder.getInterface(), { port: 7000 });
-
-console.log("Addon running on http://localhost:7000/manifest.json");
+serveHTTP(builder.getInterface(), { port: process.env.PORT || 7000 });
